3e6195e5e60a28552c95ea1de643d0f389e7ad2c
NajdAwad <najdawadh@outlook.com>
2023-07-26 09:51:20 +0300

    Add project files.

---
+++ firstApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace firstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        /* void OnButtonClicked(object sender , EventArgs argd)
+        {
+            //await Label.RelRotateTo(360, 1000);
+        }*/
+        private  void Popup_Clicked(object sender, EventArgs e)
+        {
+             PopupNavigation.Instance.PushAsync(new Popup());////Popup()
+        }
+      
+    }
+}
---
+++ firstApp/Popup.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace firstApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Popup 
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+        private void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!(string.IsNullOrEmpty(username.Text) || string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Text)))
+                    {
+                    PopupNavigation.Instance.PopAsync();
+                    Navigation.PushAsync(new SignUp());
+                }
+                else
+                {
+                    DisplayAlert("Missing Fields", "Please fill all fields ", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                 DisplayAlert("Missing Fields", "Please fill all fields ", "OK");
+
+            }
+            //NavigationPage(new signup());
+
+        }
+    }
+}
